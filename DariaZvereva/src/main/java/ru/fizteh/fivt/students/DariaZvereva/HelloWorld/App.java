0acb2ba74075986e43b7469f61b726a63c13bce7
Zvereva Daria <zverevads@gmail.com>
2015-09-22 21:21:43 +0300

    My first commit created project

---
+++ DariaZvereva/src/main/java/ru/fizteh/fivt/students/DariaZvereva/HelloWorld/App.java
@@ -0,0 +1,11 @@
+package ru.fizteh.fivt.students.DariaZvereva.HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ DariaZvereva/src/main/java/ru/fizteh/fivt/students/DariaZvereva/Reverser/Reverser.java
@@ -0,0 +1,9 @@
+package ru.fizteh.fivt.students.DariaZvereva.Reverser;
+
+
+public class Reverser {
+    public static void main(String[] args) {
+        System.out.println("LOL");
+        
+    }
+}
